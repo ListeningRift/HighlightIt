2c5623ec607c2ff57352b949fef5857e84c99ab2
ListeningRift <zhu.jingming@trs.com.cn>
2024-04-30 18:25:13 +0800

    build(vue3): format code

--- packages/vue3/scripts/build.ts
+++ packages/vue3/scripts/build.ts
@@ -2,27 +2,27 @@ import { resolve } from 'path'
 import { build } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
-import type { LibraryFormats} from 'vite';
+import type { LibraryFormats } from 'vite'
 
 // make library list, the name for folder
-const components = ['directive', 'component'];
+const components = ['directive', 'component']
 
-const libs = components.map((name) => {
+const libs = components.map(name => {
   return {
     entry: `src/${name}.ts`,
     fileName: name,
     formats: ['es'] as LibraryFormats[]
-  };
-});
+  }
+})
 
-libs.forEach(async (lib) => {
+libs.forEach(async lib => {
   await build({
     configFile: false,
     build: {
       lib,
       outDir: 'dist',
       rollupOptions: {
-        external: ['vue'],
+        external: ['vue']
       },
       emptyOutDir: false
     },
@@ -39,6 +39,6 @@ libs.forEach(async (lib) => {
           }
         }
       })
-    ],
-  });
-});
+    ]
+  })
+})
